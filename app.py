1fb8e292264f4531839d251d7b646d6ce051db3b
ricardobiselli <ricardobiselli@gmail.com>
2023-08-23 01:07:50 -0300

    completed items 1 & 2

--- app.py
+++ app.py
@@ -2,44 +2,38 @@
 
 
 import os
+from bibloteca import *
 
 print("Bienvenido!")
 respuesta = ''
 
-def menu():
-    print("1 - Gestionar Prestamo")
-    print("2 - Gestionar Devolucion")
-    print("3 - Registrar nuevo libro")
-    print("4 - Elimiar ejemplar")
-    print("5 - Mostrar ejemplares perstados")
-    print("6 - Salir")
-
 while respuesta != "salir":
     menu()
     opt = input("\n Ingrese la opción de menú: ")
-    os.system ("cls") #Limpiar pantalla
-    if opt.isnumeric():
-        if int(opt) == 1:
-            #completar
-            print()
-        elif int(opt) == 2:
-            #completar
-            print()
-        elif int(opt) == 3:
-            #completar
-            print()
-        elif int(opt) == 4:
-            #completar
-            print()
-        elif int(opt) == 5:
-            #completar
-            print()
-        elif int(opt) == 6:
-            respuesta = "salir"
-        else: print("Ingrese una opción válida")
-    else: 
-        print("Ingrese una opción numérica")
-    
-    input("Presione cualquier tecla para continuar....") # Pausa
+    os.system("cls")
+
+    if int(opt) == 1:
+        codigo_libro = input('Ingrese el código del libro: ')
+        ejemplares_prestados(codigo_libro)
+    elif int(opt) == 2:
+        codigo_libro = input('Ingrese el código del libro: ')
+        devolver_ejemplar_libro(codigo_libro)
+    elif int(opt) == 3:
+        # completar
+        print("1")
+    elif int(opt) == 4:
+        # completar
+        print("2")
+    elif int(opt) == 5:
+        # completar
+        print()
+    elif int(opt) == 6:
+        respuesta = "salir"
+    else:
+        print("Ingrese una opción válida")
+else:
+    print("Ingrese una opción numérica")
+
+input("Presione cualquier tecla para continuar....")  # Pausa
 
 print("Hasta luego!.")
--- bibloteca.py
+++ bibloteca.py
@@ -8,27 +8,79 @@ libros.append(l.libro1)
 libros.append(l.libro2)
 libros.append(l.libro3)
 
-def ejemplares_prestados():
-    # completar
+
+def menu():
+    print("1 - Gestionar Prestamo")
+    print("2 - Gestionar Devolucion")
+    print("3 - Registrar nuevo libro")
+    print("4 - Elimiar ejemplar")
+    print("5 - Mostrar ejemplares perstados")
+    print("6 - Salir")
+
+
+def ejemplares_prestados(codigo_libro):
+    libro_encontrado = None
+
+    for libro in libros:
+        if libro['cod'] == codigo_libro:
+            libro_encontrado = libro
+            break
+
+    if libro_encontrado:
+        print("El código de libro ha sido encontrado:")
+        print("Título:", libro_encontrado["titulo"])
+        print("Autor:", libro_encontrado["autor"])
+        if int(libro_encontrado["cant_ej_ad"]) - int(libro_encontrado["cant_ej_pr"]) <= 0:
+            print("no hay ejemplares disponibles para prestar")
+        else:
+            print("cantidad de ejemplares para préstamo:", int(
+                libro_encontrado["cant_ej_ad"]) - int(libro_encontrado["cant_ej_pr"]))
+    else:
+        print("El libro no existe.")
     return None
+
+
 
 def registrar_nuevo_libro():
     nuevo_libro = l.nuevo_libro()
-    #completar
+    # completar
     return None
+
 
 def eliminar_ejemplar_libro():
-    #completar
+    # completar
     return None
+
 
 def prestar_ejemplar_libro():
-    #completar
+    # completar
     return None
 
-def devolver_ejemplar_libro():
-    #completar
+
+def devolver_ejemplar_libro(codigo_libro):
+    libro_encontrado = None
+    
+    for libro in libros:
+        if libro['cod'] == codigo_libro:
+            if int(libro['cant_ej_pr']) > 0:
+                libro_encontrado = libro
+                print("El código del libro es válido, y hay: ",
+                      libro_encontrado["cant_ej_pr"], "ejemplares prestados")
+                devolucion = input("¿Desea gestionar la devolución? (s/n): ")
+                if devolucion.lower() == "s":
+                    libro['cant_ej_pr'] = str(int(libro['cant_ej_pr']) - 1)
+                    libro['cant_ej_ad'] = str(int(libro['cant_ej_ad']) + 1)
+                    print("Devolución gestionada con éxito.")
+                break
+            else:
+                print(
+                    "El código del libro es válido, pero no hay ejemplares prestados en este momento")
+                break
+    else:
+        print("ERROR, No ha ingresado un código válido")
     return None
+
 
 def nuevo_libro():
-    #completar
+    # completar
     return None
